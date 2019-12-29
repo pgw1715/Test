b096b11116a1bd29186deefddca624c8a7de077d
pgw1715 <38424666+pgw1715@users.noreply.github.com>
2019-12-29 17:51:59 +0900

    Create 1138번 : 한 줄로 서기.cpp

---
+++ 문제풀이/GREEDY/1138번 : 한 줄로 서기.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+
+vector<int> result;
+
+void setResult();
+void showResult();
+int getIdx(int);
+
+int main() {
+	int personCnt;
+	cin >> personCnt;
+
+	for (int i = 0; i < personCnt; i++) {
+		int n;
+		cin >> n;
+		v.push_back(n);
+	}
+
+	for (int i = 0; i < personCnt; i++)
+		result.push_back(0);
+
+	setResult();
+	showResult();
+
+	return 0;
+}
+
+void setResult() {
+	result[v[0]] = 1;
+	for (int i = 1; i <= v.size() - 1; i++) {
+		result[getIdx(v[i])] = i + 1;
+	}
+}
+
+int getIdx(int x) {
+	int n = 0;
+	int i = 0;
+	for (i = 0; i < result.size(); i++) {
+		if (n == x)
+			break;
+
+		if (result[i] == 0)
+			n++;
+
+	}
+	while (true) {
+		if (result[i] != 0)
+			i++;
+		else
+			break;
+	}
+
+	return i;
+}
+
+void showResult() {
+	for (int i = 0; i < result.size(); i++)
+		cout << result[i] << " ";
+}
