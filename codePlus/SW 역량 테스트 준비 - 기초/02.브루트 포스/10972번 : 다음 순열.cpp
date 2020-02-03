20f878f879bf0ac1cb0e46e7c635213c037cf28f
pgw1715 <38424666+pgw1715@users.noreply.github.com>
2020-02-03 20:26:22 +0900

    Create 10972번 : 다음 순열.cpp

---
+++ codePlus/SW 역량 테스트 준비 - 기초/02.브루트 포스/10972번 : 다음 순열.cpp
@@ -0,0 +1,29 @@
+// next_permutation 이용법 익히기
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> list;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int num;
+		scanf("%d", &num);
+		list.push_back(num);
+	}
+
+	if (next_permutation(list.begin(), list.end())) {
+		for (int i = 0; i < n; i++) {
+			printf("%d ", list[i]);
+		}
+	}
+	else {
+		printf("%d\n", -1);
+	}
+	return 0;
+}
