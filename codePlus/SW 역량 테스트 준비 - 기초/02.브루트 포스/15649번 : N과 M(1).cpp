1ae7658bd13a28b4d1be9f746e6ec41ca485c6c8
pgw1715 <38424666+pgw1715@users.noreply.github.com>
2020-02-10 17:02:06 +0900

    Create 15649번 : N과 M(1).cpp

---
+++ codePlus/SW 역량 테스트 준비 - 기초/02.브루트 포스/15649번 : N과 M(1).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void makePermutation(int, int, int, int*, bool*);
+void printPermutation(int*, int);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	vector<int> numList;
+	int n, m;
+	cin >> n >> m;
+
+	int permutation[8];
+	bool check[8] = {false,};
+	makePermutation(0, n, m, permutation, check);
+	return 0;
+}
+
+void makePermutation(int idx, int n, int m, int* permutation, bool* check) {
+	if (idx == m) {
+		printPermutation(permutation, m);
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (check[i]) continue;
+		permutation[idx] = i + 1;
+		check[i] = true;
+		makePermutation(idx + 1, n, m, permutation, check);
+		check[i] = false;
+	}
+}
+
+void printPermutation(int* permutation, int m) {
+	for (int i = 0; i < m ;i++) {
+		cout << permutation[i] << ' ';
+	}
+	cout << '\n';
+}
