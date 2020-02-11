11ecf870c66d0da2ae34ae24c81bc3a12c64a5d9
pgw1715 <38424666+pgw1715@users.noreply.github.com>
2020-02-11 17:31:29 +0900

    Create 15666번 : N과 M (12).cpp

---
+++ codePlus/SW 역량 테스트 준비 - 기초/02.브루트 포스/15666번 : N과 M (12).cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void makePermutation(vector<vector<int>>&, vector<int>&, int*, int, int, int, int);
+void printUniqueList(vector<vector<int>>&);
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n,m;
+	cin>>n>>m;
+	vector<int> numList;
+	for(int i=0;i<n;i++){
+		int num;
+		cin>>num;
+		numList.push_back(num);
+	}
+	sort(numList.begin(), numList.end());
+
+	vector<vector<int>> uniqueList;
+	int permutation[8];
+	makePermutation(uniqueList, numList, permutation, n, m, 0, 0);
+
+	sort(uniqueList.begin(), uniqueList.end());
+	uniqueList.erase(unique(uniqueList.begin(), uniqueList.end()), uniqueList.end());
+	printUniqueList(uniqueList);
+	return 0;
+}
+
+void makePermutation(vector<vector<int>>& uniqueList, vector<int>& numList, int* permutation, int n, int m, int pick, int start){
+	if(pick == m){
+		vector<int> tmp;
+		for(int i=0;i<m;i++){
+			tmp.push_back(permutation[i]);
+		}
+		uniqueList.push_back(tmp);
+		return;
+	}
+	for(int i=start;i<n;i++){
+		permutation[pick] = numList[i];
+		makePermutation(uniqueList, numList, permutation, n, m, pick+1, i);
+	}
+}
+
+void printUniqueList(vector<vector<int>>& uniqueList){
+	for(int i=0;i<uniqueList.size();i++){
+		for(int j=0;j<uniqueList[i].size();j++){
+			cout<<uniqueList[i][j]<<' ';
+		}
+		cout<<'\n';
+	}
+}
